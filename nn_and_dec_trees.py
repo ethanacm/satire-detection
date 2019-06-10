98ae12235b9baf240a1967a481c9d2053290e1c5
nmannes <nmannes@gmail.com>
2019-06-10 15:33:07 -0500

    deleted file

--- nn_and_dec_trees.py
+++
@@ -1,9 +0,0 @@
-    def sentence_to_vector(self, sentence, features):
-        sentence_features = {}
-        for feature in features:
-            sentence_features.update(feature(sentence))
-        sorted_keys = sorted(features)
-        arr = np.ndarray(len(features))
-        for i, key in enumerate(sorted_keys):
-            arr[i] = sentence_features[key]
-        return arr
